a9dfd429f1d3fd5d8a28d9d54119b29a1b1e6982
YamenJun <armorn@163.com>
2025-03-03 20:31:21 +0800

    update

--- test/test_add_methods.py
+++ test/test_add_methods.py
@@ -1,9 +1,11 @@
-from mian import add
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from mian import add
 
 def test_add_success():
     assert add(1, 2) == 3
-
 
 def test_add_fail():
     assert add(1, 2) == 4
